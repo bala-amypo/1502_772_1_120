32496eb2c9edcf2f4c5c7542110f7393a2379562
bala-amypo <bala@amypo.in>
2025-12-29 03:10:23 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ApiUsageLogServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ApiUsageLogServiceImpl.java
@@ -1,72 +1,9 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.dto.*;
-import com.example.demo.entity.UserAccount;
-import com.example.demo.exception.BadRequestException;
-import com.example.demo.repository.UserAccountRepository;
-import com.example.demo.security.JwtUtil;
-import com.example.demo.service.AuthService;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import com.example.demo.service.ApiUsageLogService;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
-
 @Service
-public class AuthServiceImpl implements AuthService {
-
-    private final UserAccountRepository userRepo;
-    private final PasswordEncoder passwordEncoder;
-    private final AuthenticationManager authenticationManager;
-    private final JwtUtil jwtUtil;
-
-    public AuthServiceImpl(
-            UserAccountRepository userRepo,
-            PasswordEncoder passwordEncoder,
-            AuthenticationManager authenticationManager,
-            JwtUtil jwtUtil
-    ) {
-        this.userRepo = userRepo;
-        this.passwordEncoder = passwordEncoder;
-        this.authenticationManager = authenticationManager;
-        this.jwtUtil = jwtUtil;
-    }
-
-    @Override
-    public UserAccount register(RegisterRequestDto dto) {
-        if (userRepo.existsByEmail(dto.getEmail())) {
-            throw new BadRequestException("Email already exists");
-        }
-
-        UserAccount user = new UserAccount();
-        user.setEmail(dto.getEmail());
-        user.setPassword(passwordEncoder.encode(dto.getPassword()));
-        user.setRole(dto.getRole());
-
-        return userRepo.save(user);
-    }
-
-    @Override
-    public AuthResponseDto login(AuthRequestDto dto) {
-
-        UserAccount user = userRepo.findByEmail(dto.getEmail())
-                .orElseThrow(() -> new BadRequestException("Invalid credentials"));
-
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        dto.getEmail(),
-                        dto.getPassword()
-                )
-        );
-
-        String token = jwtUtil.generateToken(
-                Map.of("role", user.getRole()),
-                user.getEmail()
-        );
-
-        AuthResponseDto response = new AuthResponseDto();
-        response.setToken(token);
-        return response;
-    }
+public class ApiUsageLogServiceImpl implements ApiUsageLogService {
+    
 }
