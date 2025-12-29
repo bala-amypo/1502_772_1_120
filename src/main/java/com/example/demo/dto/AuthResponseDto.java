64fb47185814382bae69a3912d1fd1a3cdec0da3
bala-amypo <bala@amypo.in>
2025-12-29 02:00:15 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthResponseDto.java
+++ src/main/java/com/example/demo/dto/AuthResponseDto.java
@@ -2,13 +2,18 @@ package com.example.demo.dto;
 
 public class AuthResponseDto {
 
+    private String email;
     private String token;
 
     public AuthResponseDto() {
     }
 
-    public AuthResponseDto(String token) {
-        this.token = token;
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getToken() {
